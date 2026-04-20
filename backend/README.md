7d9cc8d0d0673b84e1fa2d6f71679c742b2d471b
LeanAranda <arandaleandro2003@gmail.com>
2026-04-20 11:10:51 -0300

    Config inicial Backend NestJS con Prisma y PostgreSQL

---
+++ backend/README.md
@@ -0,0 +1,7 @@
+# Recipes App - Backend
+
+## Stack
+- [NestJS](https://nestjs.com/)                     - Framework backend en Node.js
+- [Prisma](https://www.prisma.io/)                  - ORM para base de datos
+- [PostgreSQL](https://www.postgresql.org/)         - Base de datos
+- [TypeScript](https://www.typescriptlang.org/)     - Tipado estático
---
+++ backend/prisma/migrations/20260420134037_init/migration.sql
@@ -0,0 +1,44 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "firstName" TEXT NOT NULL,
+    "lastName" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Recipe" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "ingredients" TEXT NOT NULL,
+    "userId" INTEGER NOT NULL,
+    "imageUrl" TEXT,
+
+    CONSTRAINT "Recipe_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Rating" (
+    "id" SERIAL NOT NULL,
+    "score" INTEGER NOT NULL,
+    "userId" INTEGER NOT NULL,
+    "recipeId" INTEGER NOT NULL,
+
+    CONSTRAINT "Rating_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- AddForeignKey
+ALTER TABLE "Recipe" ADD CONSTRAINT "Recipe_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Rating" ADD CONSTRAINT "Rating_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Rating" ADD CONSTRAINT "Rating_recipeId_fkey" FOREIGN KEY ("recipeId") REFERENCES "Recipe"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
